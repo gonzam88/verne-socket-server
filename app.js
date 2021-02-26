76929380017b587c3cf3488c2c140f4d6148b054
gonza <gonzamoiguer@gmail.com>
2021-02-26 13:31:25 -0300

    mejor sistema color. mejor navbar a estandar Bulma

--- app.js
+++ app.js
@@ -22,35 +22,42 @@ var io = require('socket.io')(server);
 //passport config:
 require('./config/passport')(passport)
 //mongoose
-mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
-.then(() => console.log('connected to db'))
-.catch((err)=> console.log(err));
+mongoose.connect('mongodb://localhost/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('connected to db'))
+  .catch((err) => console.log(err));
 
 //EJS
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(expressEjsLayout);
 
 //BodyParser
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({
+  extended: false
+}));
 
 //express session
-var sessionStore = MongoStore.create({ mongoUrl: 'mongodb://localhost/sessions'})
+var sessionStore = MongoStore.create({
+  mongoUrl: 'mongodb://localhost/sessions'
+})
 
 app.use(session({
   // original
-    // secret : 'secret',
-    // resave : true,
-    // saveUninitialized : true
+  // secret : 'secret',
+  // resave : true,
+  // saveUninitialized : true
 
-    //middleware para que funque con socketio
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
+  //middleware para que funque con socketio
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
     // secure: process.env.ENVIRONMENT !== 'development' && process.env.ENVIRONMENT !== 'test',
     maxAge: 2419200000
-    },
-    secret: process.env.SECRET_KEY_BASE
+  },
+  secret: process.env.SECRET_KEY_BASE
 }));
 
 passport.serializeUser(function(user, done) {
@@ -67,16 +74,16 @@ passport.deserializeUser(function(id, done) {
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-app.use((req,res,next)=> {
+app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
-  res.locals.error  = req.flash('error');
+  res.locals.error = req.flash('error');
   next();
 })
 app.use(express.static('public'));
 
 //Routes
-app.use('/',require('./routes/index'));
+app.use('/', require('./routes/index'));
 // app.use('/users',require('./routes/users'));
 
 app.listen(process.env.EXPRESSPORT);
@@ -113,7 +120,7 @@ var players = []
 
 
 io.on('connection', (socket) => {
-	// user data from the socket.io passport middleware
+  // user data from the socket.io passport middleware
   if (socket.request.user && socket.request.user.logged_in) {
     // usuario logueado
     let userId = socket.request.user._id
@@ -125,140 +132,56 @@ io.on('connection', (socket) => {
       color: socket.request.user.color
     }
 
-      //socket.emit("id", userId) // Le aviso cual es su id
-      // le aviso su id y color
-      socket.emit("loginData", newPlayerData)
+    //socket.emit("id", userId) // Le aviso cual es su id
+    // le aviso su id y color
+    socket.emit("loginData", newPlayerData)
 
-      for(var p in players){
-        socket.emit("newPlayer", players[p]) // le mando cada uno de los players existentes
-      }
-      players[userId] = newPlayerData // Agrego este player al array
+    for (var p in players) {
+      socket.emit("newPlayer", players[p]) // le mando cada uno de los players existentes
+    }
+    players[userId] = newPlayerData // Agrego este player al array
 
-      socket.broadcast.emit("newPlayer", players[userId]) // Les aviso al resto del nuevo jugador
+    socket.broadcast.emit("newPlayer", players[userId]) // Les aviso al resto del nuevo jugador
 
 
-      console.log("Nuevo player",userId, "Players online", Object.keys(players).length)
+    console.log("Nuevo player", userId, "Players online", Object.keys(players).length)
+    console.log(players)
+
+
+    socket.on('disconnect', () => {
+      delete players[userId] // Elimino al player del array
+      socket.broadcast.emit("deletePlayer", userId) // Les aviso al resto que este se fue
+
+      console.log("Usuario desconectado", "Usuarios conectados", Object.keys(players).length)
       console.log(players)
+    })
 
+    socket.on("playerUpdate", (data) => {
+      players[data.id][data.parameter] = data.value; // Guardo en en este jugador
+      console.log(data.parameter, data.value)
 
-      socket.on('disconnect', () => {
-        delete players[userId] // Elimino al player del array
-        socket.broadcast.emit("deletePlayer", userId) // Les aviso al resto que este se fue
-
-        console.log("Usuario desconectado", "Usuarios conectados", Object.keys(players).length)
-        console.log(players)
-      })
-
-      socket.on("playerUpdate", (data)=>{
-        players[data.id][data.parameter] = data.value; // Guardo en en este jugador
-        console.log(data.parameter, data.value)
-
-        // Si cambió el color lo guardo en su DB
-        if(data.parameter == "color"){
-            User.findOne({ _id: userId}, function(err, userDb) {
-                if (userDb){
-                  userDb.color = data.value;
-                  userDb.save(function (err) {
-                      if (err)
-                      {
-                          // TODO: Handle the error!
-                      }
-                      // console.log("color guardado")
-                  });
-                }
-            })
-        }
-        socket.broadcast.emit("otherUpdate", data) // Le aviso a los demas players conectados
-      })
-
-
-      // SERVIDOR COSAS VIEJAS
-      // socket.on("valueChange", (data) =>{
-      //   console.log(data)
-      //   socket.broadcast.emit(
-      //      'valueChange',
-      //      {
-      //        parameter: data.parameter,
-      //        value: data.value
-      //      }
-      //    );
-      // })
-      //
-      // socket.on("clickedBut", (data) =>{
-      //   console.log(data)
-      //   socket.broadcast.emit(
-      //      'clickedBut',
-      //      {
-      //        parameter: data.parameter,
-      //        value: data.value
-      //      }
-      //    );
-      // })
-
-
+      // Si cambió el color lo guardo en su DB
+      if (data.parameter == "color") {
+        User.findOne({
+          _id: userId
+        }, function(err, userDb) {
+          if (userDb) {
+            userDb.color = data.value;
+            userDb.save(function(err) {
+              if (err) {
+                // TODO: Handle the error!
+              }
+              // console.log("color guardado")
+            });
+          }
+        })
+      }
+      socket.broadcast.emit("otherUpdate", data) // Le aviso a los demas players conectados
+    })
   }
-
 })
-// io.on('connection', (socket) => {
-//   console.log("user connected")
-//   console.log()
-//
-//   socket.emit("id", socket.id) // Le aviso cual es su id
-//   for(var p in players){
-//     socket.emit("newPlayer", players[p]) // le mando cada uno de los players existentes
-//   }
-//   players[socket.id] = // Agrego este player al array
-//     {
-//       id:  socket.id,
-//       nombre: "",
-//       color: "#000000",
-//   }
-//   socket.broadcast.emit("newPlayer", players[socket.id]) // Les aviso al resto del nuevo jugador
-//
-//
-//
-//   console.log("Nuevo player",socket.id, "Players online", Object.keys(players).length)
-//   console.log(players)
-//
-//
-//
-//   socket.on('disconnect', () => {
-//     delete players[socket.id] // Elimino al player del array
-//     socket.broadcast.emit("deletePlayer", socket.id) // Les aviso al resto que este se fue
-//
-//     console.log("Usuario desconectado", "Usuarios conectados", Object.keys(players).length)
-//     console.log(players)
-//   })
-//
-//   socket.on("playerUpdate", (data)=>{
-//     players[data.id][data.parameter] = data.value // Guardo en en este jugador
-//     console.log(data.value)
-//     socket.broadcast.emit("otherUpdate", data) // Le aviso a los demas players conectados
-//   })
-//
-//
-//   // SERVIDOR COSAS VIEJAS
-//   socket.on("valueChange", (data) =>{
-//     console.log(data)
-//     socket.broadcast.emit(
-//        'valueChange',
-//        {
-//          parameter: data.parameter,
-//          value: data.value
-//        }
-//      );
-//   })
-//
-//   socket.on("clickedBut", (data) =>{
-//     console.log(data)
-//     socket.broadcast.emit(
-//        'clickedBut',
-//        {
-//          parameter: data.parameter,
-//          value: data.value
-//        }
-//      );
-//   })
-// });
 
- console.log(`App is listening on port ${listener.address().port}`);
+
+
+
+console.log(`App is listening on port ${listener.address().port}`);
--- routes/index.js
+++ routes/index.js
@@ -1,121 +1,145 @@
 const express = require('express');
-const router  = express.Router();
-const {ensureAuthenticated} = require('../config/auth')
+const router = express.Router();
+const {
+  ensureAuthenticated
+} = require('../config/auth')
 const User = require("../models/user");
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 // landing page
-router.get('/', (req,res)=>{
-  if(req.isAuthenticated()) {
-      res.redirect('/experiencia');
-  }else{
-      res.render('home');
+router.get('/', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.redirect('/experiencia');
+  } else {
+    res.render('home');
   }
 
 })
 
 // dashboard
-router.get('/experiencia',ensureAuthenticated,(req,res)=>{
-    res.render('experiencia',{
-        user: req.user
-    });
+router.get('/experiencia', ensureAuthenticated, (req, res) => {
+  res.render('experiencia', {
+    user: req.user
+  });
 })
 
 
 //login handle
-router.get('/login',(req,res)=>{
+router.get('/login', (req, res) => {
   res.render('login');
 })
-router.get('/registrar',(req,res)=>{
+router.get('/registrar', (req, res) => {
   res.render('registrar')
 })
 
 //Register handle
-router.post('/login',(req,res,next)=>{
-  passport.authenticate('local',{
-      successRedirect : '/experiencia',
-      failureRedirect: '/login',
-      failureFlash : true
-  })(req,res,next)
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/experiencia',
+    failureRedirect: '/login',
+    failureFlash: true
+  })(req, res, next)
 })
 
 //register post handle
-  router.post('/registrar',(req,res)=>{
-    const {name,email, password, password2} = req.body;
-    let errors = [];
-    console.log(' Name ' + name+ ' email :' + email+ ' pass:' + password);
-    if(!name || !email || !password || !password2) {
-        errors.push({msg : "Por favor llená todos los campos"})
-    }
-    //check if match
-    if(password !== password2) {
-        errors.push({msg : "Contraseñas no coinciden"});
-    }
+router.post('/registrar', (req, res) => {
+  const {
+    name,
+    email,
+    password,
+    password2
+  } = req.body;
+  let errors = [];
+  console.log(' Name ' + name + ' email :' + email + ' pass:' + password);
+  if (!name || !email || !password || !password2) {
+    errors.push({
+      msg: "Por favor llená todos los campos"
+    })
+  }
+  //check if match
+  if (password !== password2) {
+    errors.push({
+      msg: "Contraseñas no coinciden"
+    });
+  }
 
-    //check if password is more than 6 characters
-    if(password.length < 6 ) {
-        errors.push({msg : 'Contraseña debe tener al menos 6 caracteres'})
-    }
-    if(errors.length > 0 ) {
+  //check if password is more than 6 characters
+  if (password.length < 6) {
+    errors.push({
+      msg: 'Contraseña debe tener al menos 6 caracteres'
+    })
+  }
+  if (errors.length > 0) {
     res.render('registrar', {
-        errors : errors,
-        name : name,
-        email : email,
-        password : password,
-        password2 : password2})
-     } else {
-        //validation passed
-       User.findOne({email : email}).exec((err,user)=>{
-        console.log(user);
-        if(user) {
-            errors.push({msg: 'Ya existe una cuenta con ese email'});
-            res.render('registrar',{errors,name,email,password,password2})
-           } else {
-            const newUser = new User({
-                name : name,
-                email : email,
-                password : password
-            });
+      errors: errors,
+      name: name,
+      email: email,
+      password: password,
+      password2: password2
+    })
+  } else {
+    //validation passed
+    User.findOne({
+      email: email
+    }).exec((err, user) => {
+      console.log(user);
+      if (user) {
+        errors.push({
+          msg: 'Ya existe una cuenta con ese email'
+        });
+        res.render('registrar', {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        })
+      } else {
+        const newUser = new User({
+          name: name,
+          email: email,
+          password: password
+        });
 
-            //hash password
-            bcrypt.genSalt(10,(err,salt)=>
-            bcrypt.hash(newUser.password,salt,
-                (err,hash)=> {
-                    if(err) throw err;
-                        //save pass to hash
-                        newUser.password = hash;
-                    //save user
-                    newUser.save()
-                    .then((value)=>{
-                        console.log(value)
-                        // req.flash('success_msg','Ya estás registrado!');
-                        // res.redirect('/login');
-                        // TODO autologin acá
-                        // passport.authenticate('local',{
-                        //     successRedirect : '/experiencia',
-                        //     failureRedirect: '/login',
-                        //     failureFlash : true
-                        // })(req,res,next)
-                        req.login(newUser, function (err) {
-                            if ( ! err ){
-                                res.redirect('/experiencia');
-                            } else {
-                                //handle error
-                            }
-                        })
-                    })
-                    .catch(value=> console.log(value));
+        //hash password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt,
+            (err, hash) => {
+              if (err) throw err;
+              //save pass to hash
+              newUser.password = hash;
+              //save user
+              newUser.save()
+                .then((value) => {
+                  console.log(value)
+                  // req.flash('success_msg','Ya estás registrado!');
+                  // res.redirect('/login');
+                  // TODO autologin acá
+                  // passport.authenticate('local',{
+                  //     successRedirect : '/experiencia',
+                  //     failureRedirect: '/login',
+                  //     failureFlash : true
+                  // })(req,res,next)
+                  req.login(newUser, function(err) {
+                    if (!err) {
+                      res.redirect('/experiencia');
+                    } else {
+                      //handle error
+                    }
+                  })
+                })
+                .catch(value => console.log(value));
 
-                }));
-             }
-       })
-    }
-  })
+            }));
+      }
+    })
+  }
+})
 
 //logout
-router.get('/logout',(req,res)=>{
+router.get('/logout', (req, res) => {
   req.logout();
-  req.flash('success_msg','Deslogueado');
+  req.flash('success_msg', 'Deslogueado');
   res.redirect('/login');
 })
 
