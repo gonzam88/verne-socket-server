41c69c694a49ae0ae63e98fc6ddf57118aa77cfd
gonza <gonzamoiguer@gmail.com>
2021-02-25 23:58:47 -0300

    pre npm update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
@@ -6,6 +7,18 @@ const expressEjsLayout = require('express-ejs-layouts')
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require("passport");
+
+var sessionStore = require("connect-mongoose-session-store")(express)
+
+
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+var http = require('http');
+var server = http.createServer(app);
+var port = process.env.PORT;
+var listener = server.listen(port);
+var io = require('socket.io')(server);
+// TODO agregar CORS al servidor de socket
 
 //passport config:
 require('./config/passport')(passport)
@@ -17,14 +30,28 @@ mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTo
 //EJS
 app.set('view engine','ejs');
 app.use(expressEjsLayout);
+
 //BodyParser
 app.use(express.urlencoded({extended : false}));
+
 //express session
 app.use(session({
-    secret : 'secret',
-    resave : true,
-    saveUninitialized : true
+  // original
+    // secret : 'secret',
+    // resave : true,
+    // saveUninitialized : true
+
+    //middleware para que funque con socketio
+    store: sessionstore.createSessionStore({ type: 'mongodb'}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+    // secure: process.env.ENVIRONMENT !== 'development' && process.env.ENVIRONMENT !== 'test',
+    maxAge: 2419200000
+    },
+    secret: process.env.SECRET_KEY_BASE
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
@@ -40,4 +67,109 @@ app.use(express.static('public'));
 app.use('/',require('./routes/index'));
 // app.use('/users',require('./routes/users'));
 
-app.listen(3000);
+app.listen(process.env.EXPRESSPORT);
+
+
+// **************
+// SOCKET SERVER
+// **************
+
+// share session with express
+io.use(passportSocketIo.authorize({
+  key: 'connect.sid',
+  secret: process.env.SECRET_KEY_BASE,
+  store: sessionstore,
+  passport: passport,
+  cookieParser: cookieParser
+}));
+
+// var eventSocket = io.of('/events');
+// // on connection event
+// eventSocket.on('connection', function(socket) {
+//
+//   // example 'event1', with an object. Could be triggered by socket.io from the front end
+//   socket.on('event1', function(eventData) {
+//   	// user data from the socket.io passport middleware
+//     if (socket.request.user && socket.request.user.logged_in) {
+//       console.log(socket.request.user);
+//     }
+//   });
+// });
+
+var players = []
+
+
+io.on('connection', (socket) => {
+  console.log("user connected")
+
+  	// user data from the socket.io passport middleware
+    if (socket.request.user && socket.request.user.logged_in) {
+      console.log(socket.request.user);
+    }else{
+      console.log("usuario no logeado")
+    }
+
+})
+// io.on('connection', (socket) => {
+//   console.log("user connected")
+//   console.log()
+//
+//   socket.emit("id", socket.id) // Le aviso cual es su id
+//   for(var p in players){
+//     socket.emit("newPlayer", players[p]) // le mando cada uno de los players existentes
+//   }
+//   players[socket.id] = // Agrego este player al array
+//     {
+//       id:  socket.id,
+//       nombre: "",
+//       color: "#000000",
+//   }
+//   socket.broadcast.emit("newPlayer", players[socket.id]) // Les aviso al resto del nuevo jugador
+//
+//
+//
+//   console.log("Nuevo player",socket.id, "Players online", Object.keys(players).length)
+//   console.log(players)
+//
+//
+//
+//   socket.on('disconnect', () => {
+//     delete players[socket.id] // Elimino al player del array
+//     socket.broadcast.emit("deletePlayer", socket.id) // Les aviso al resto que este se fue
+//
+//     console.log("Usuario desconectado", "Usuarios conectados", Object.keys(players).length)
+//     console.log(players)
+//   })
+//
+//   socket.on("playerUpdate", (data)=>{
+//     players[data.id][data.parameter] = data.value // Guardo en en este jugador
+//     console.log(data.value)
+//     socket.broadcast.emit("otherUpdate", data) // Le aviso a los demas players conectados
+//   })
+//
+//
+//   // SERVIDOR COSAS VIEJAS
+//   socket.on("valueChange", (data) =>{
+//     console.log(data)
+//     socket.broadcast.emit(
+//        'valueChange',
+//        {
+//          parameter: data.parameter,
+//          value: data.value
+//        }
+//      );
+//   })
+//
+//   socket.on("clickedBut", (data) =>{
+//     console.log(data)
+//     socket.broadcast.emit(
+//        'clickedBut',
+//        {
+//          parameter: data.parameter,
+//          value: data.value
+//        }
+//      );
+//   })
+// });
+
+ console.log(`Socket is listening on port ${listener.address().port}`);
